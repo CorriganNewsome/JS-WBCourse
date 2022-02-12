353ed2e650a15a03bd0d48cf2865f3a55ad45fb1
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-11 22:07:23 -0600

    functions

---
+++ playground/functions.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let calculateBill = (billAmount, taxRate) => {
+  console.log("Running Calculate Bill!");
+  const total = billAmount * (1 + taxRate);
+  return total;
+};
+
+const myTotal = calculateBill(100, 0.13);
+console.log(myTotal);
+
+let doctorize = (name) => {
+  return `Dr. ${name}`;
+};
+let yell = (name) => {
+  return `HEY ${name.toUpperCase()}`;
+};
